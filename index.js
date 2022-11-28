4e8982f6cfa04a1b68410313aee281c2d2027d22
Jisan-Hasan <jisan.jh5@gmail.com>
2022-11-28 18:16:54 +0600

    mongodb setup completed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
@@ -12,11 +13,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// database setup
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
 
-
-
-
-
+// run function & api endpoint
+async function run() {
+    try {
+        const usersCollection = client.db(recycleHub - db).collection("users");
+    } finally {
+    }
+}
+run().catch((error) => console.log(error));
 
 app.get("/", (req, res) => {
     res.send("Server is running!");
